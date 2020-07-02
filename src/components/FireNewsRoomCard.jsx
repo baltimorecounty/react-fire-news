4f46444c8dfd5c546f670ea52c1e88397dc18386
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-07-02 15:11:55 -0400

    Added fire news room

---
+++ src/components/FireNewsRoomCard.jsx
@@ -0,0 +1,55 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardFooter,
+} from "@baltimorecounty/dotgov-components";
+
+import FireNewsThumbnail from "./FireNewsThumbnail";
+import React from "react";
+
+const NewsRoomCard = (props) => {
+  const {
+    publishDate,
+    title,
+    articleSummary,
+    thumbnail,
+    thumbnailAltText,
+    url,
+  } = props;
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const published = new Date(publishDate).toLocaleDateString("en-US", options);
+
+  return (
+    <Card className="text-left">
+      <h2>
+        <a href={url}>{title}</a>
+      </h2>
+      <CardContent>
+        <div className="row">
+          <div className="col-sm-3 col-xs-12 text-center">
+            <FireNewsThumbnail
+              thumbnail={thumbnail}
+              thumbnailAltText={thumbnailAltText}
+            />
+          </div>
+          <div className="col-sm-9 col-xs-12">
+            <p>{published}</p>
+            <p>{articleSummary}</p>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="text-right">
+        <Button as="a" text="Details" href={url} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default NewsRoomCard;
